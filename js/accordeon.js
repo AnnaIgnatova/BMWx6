2aa95f6a15816b746ee783dc9297e71b3903700b
Anna <61065956+AnnaIgnatova@users.noreply.github.com>
2021-05-18 16:18:21 +0500

    init repo

---
+++ js/accordeon.js
@@ -0,0 +1,20 @@
+const featureLinkElems = document.querySelectorAll('.feature__link');
+const featureSubElems = document.querySelectorAll('.feature-sub');
+
+featureLinkElems.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        if (btn.classList.contains('feature__link_active')) {
+            btn.classList.remove('feature__link_active');
+            featureSubElems[index].classList.add('hidden');
+        } else {
+            featureSubElems.forEach(item => {
+                item.classList.add('hidden');
+            });
+            featureLinkElems.forEach(item => {
+                item.classList.remove('feature__link_active');
+            });
+            featureSubElems[index].classList.remove('hidden');
+            btn.classList.add('feature__link_active'); 
+        } 
+    });
+});
