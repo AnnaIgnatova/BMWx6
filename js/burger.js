4b72c2614d1a527248407851df95375e4764ee93
Anna Ignatova <missjellx@gmail.com>
2021-05-20 08:55:14 +0300

    feat: add burger menu and modal windows

---
+++ js/burger.js
@@ -0,0 +1,17 @@
+const menuElem = document.querySelector('.menu');
+const humburgerElem = document.querySelector('.humburger-menu');
+
+const toggleMenu = () => {
+  menuElem.classList.toggle('menu-active');
+  humburgerElem.classList.toggle('humburger-menu-active');
+}
+
+const closeMenu = (event) => {
+  const target = event.target;
+  if (target.classList.contains('menu-list__link')) {
+    menuElem.classList.remove('menu-active');
+    humburgerElem.classList.remove('humburger-menu-active');
+  }
+}
+humburgerElem.addEventListener('click', toggleMenu);
+menuElem.addEventListener('click', closeMenu);
---
+++ js/modal.js
@@ -0,0 +1,24 @@
+const moreElem = document.querySelectorAll('.more');
+const modalElem = document.querySelector('.modal');
+
+const openModal = () => {
+  modalElem.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modalElem.classList.add('hidden');
+};
+
+moreElem.forEach(btn => {
+  btn.addEventListener('click', openModal)
+});
+
+modalElem.addEventListener('click', (event) => {
+  const target = event.target;
+
+  if (target.classList.contains('overlay') || 
+      target.classList.contains('modal__close')) {
+      closeModal();
+  }
+
+});
